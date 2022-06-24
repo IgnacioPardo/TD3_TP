26730b96e01856008b93cff41234af130dfb261a
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2022-06-24 13:41:13 -0300

    Weird indentation

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,13 +1,13 @@
-	#include "Usuario.h"
+#include "Usuario.h"
 
 using namespace std;
 
 Usuario::Usuario(int id_, string alias_){
-		this->_id = id_;
-		this->_alias = alias_;
-		this->_amigos = *(new set<Usuario*>);
-		this->_alias_amigos = *(new set<string>);
-		this->_cantidad_amigos = 0;
+	this->_id = id_;
+	this->_alias = alias_;
+	this->_amigos = *(new set<Usuario*>);
+	this->_alias_amigos = *(new set<string>);
+	this->_cantidad_amigos = 0;
 }
 
 
@@ -23,33 +23,33 @@ int Usuario::obtener_id() const{
 
 
 void Usuario::amigar_usuario(Usuario *usr){
-		//Post: El conjunto this->_amigos tiene un elemento mas: usr
-		// 			El conjunto this->_alias_amigos tiene un elemento mas: usr._alias
-		//			this->_cantidad_amigos = |this->_amigos| = |this->_alias_amigos|
-		
-		this->_cantidad_amigos += 1 - this->_amigos.count(usr); //O(log n)
-		this->_amigos.insert(usr); //O(log n)
+	//Post: El conjunto this->_amigos tiene un elemento mas: usr
+	//		El conjunto this->_alias_amigos tiene un elemento mas: usr._alias
+	//		this->_cantidad_amigos = |this->_amigos| = |this->_alias_amigos|
+	
+	this->_cantidad_amigos += 1 - this->_amigos.count(usr); //O(log n)
+	this->_amigos.insert(usr); //O(log n)
 	  this->_alias_amigos.insert(usr->obtener_alias()); //O(log n)
 }
 
 void Usuario::desamigar_usuario(Usuario *usr){
-		//Post: El conjunto this->_amigos tiene un elemento menos: usr
-		// 			El conjunto this->_amigos_amigos tiene un elemento menos: usr._alias
-		//			this->_cantidad_amigos = |this->_amigos| = |this->_alias_amigos|
-		
-		this->_cantidad_amigos -= this->_amigos.count(usr); //O(log n)
-		this->_amigos.erase(usr); //O(log n)
-		this->_alias_amigos.erase(usr->obtener_alias()); //O(log n)
+	//Post: El conjunto this->_amigos tiene un elemento menos: usr
+	//		El conjunto this->_amigos_amigos tiene un elemento menos: usr._alias
+	//		this->_cantidad_amigos = |this->_amigos| = |this->_alias_amigos|
+	
+	this->_cantidad_amigos -= this->_amigos.count(usr); //O(log n)
+	this->_amigos.erase(usr); //O(log n)
+	this->_alias_amigos.erase(usr->obtener_alias()); //O(log n)
 }
 
 void Usuario::desamigar_todos(){
-		//Post: El conjunto this->_amigos es el conjunto vacio.
-		// 			El conjunto this->_amigos_amigos es el conjunto vacio.
-		//			this->_cantidad_amigos = |this->_amigos| = |this->_alias_amigos| = 0
+	//Post: El conjunto this->_amigos es el conjunto vacio.
+	//		El conjunto this->_amigos_amigos es el conjunto vacio.
+	//		this->_cantidad_amigos = |this->_amigos| = |this->_alias_amigos| = 0
 
-		this->_cantidad_amigos = 0;
-		this->_amigos = *(new set<Usuario*>); //O(log n)
-		this->_alias_amigos = *(new set<string>); //O(log n)
+	this->_cantidad_amigos = 0;
+	this->_amigos = *(new set<Usuario*>); //O(log n)
+	this->_alias_amigos = *(new set<string>); //O(log n)
 }
 
 const set<string> & Usuario::obtener_amigos() const{
